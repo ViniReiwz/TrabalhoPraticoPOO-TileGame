9df8720f1d0b0a732e4e30a0bb038d8124529262
CarlosZecchi <carlosddzecchi@gmail.com>
2025-11-18 14:41:56 -0300

    Delete TrabalhoPraticoPOO-TileGame-main directory

--- TrabalhoPraticoPOO-TileGame-main/Implementação/src/Modelo/ColetavelMult.java
+++
@@ -1,9 +0,0 @@
-package Modelo;
-
-public class ColetavelMult extends Coletavel 
-{
-    public ColetavelMult(String NomeImagem, int linha, int coluna)
-    {
-        super(NomeImagem,linha,coluna);
-    }
-}
